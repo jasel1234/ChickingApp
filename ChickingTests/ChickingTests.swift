4ea5c8c76301458b9b4282361247bc0528074c33
sonup1296 <53327341+sonup1296@users.noreply.github.com>
2020-05-16 11:17:53 +0530

    Initial Commit

---
+++ ChickingTests/ChickingTests.swift
@@ -0,0 +1,34 @@
+//
+//  ChickingTests.swift
+//  ChickingTests
+//
+//  Created by Nagaraj R on 16/05/20.
+//  Copyright © 2020 Nagaraj R. All rights reserved.
+//
+
+import XCTest
+@testable import Chicking
+
+class ChickingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
